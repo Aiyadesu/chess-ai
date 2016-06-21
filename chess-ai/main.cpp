70afe91145f0fd68b0aee6f6af10a6efd99cb858
Richard Lai <richard08192@hotmail.com>
2016-06-21 21:44:29 +1000

    Added Qt Resource directory and QML file to show a dummy screen.

--- chess-ai/main.cpp
+++ chess-ai/main.cpp
@@ -1,8 +1,31 @@
-#include <QCoreApplication>
+//#include <QCoreApplication>
+
+#include <iostream>
+#include "chesspiece.h"
+#include "pawn.h"
+#include <QString>
+#include "board.h"
+
+#include <QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QQuickView>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+
+    Board* boardptr = new Board();
+
+    QGuiApplication a(argc, argv); // GUI application
+
+    //QQuickView view;
+    //view.setSource(QUrl(QStringLiteral("qrc:/GameWindow.qml")));
+    //view.show();
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/GameWindow.qml")));
+
 
     return a.exec();
 }
